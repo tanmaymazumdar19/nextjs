e61e3c6696d00d2443826732d3020a3c3df03de8
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2023-12-14 12:24:11 +0530

    (feat)[font]: Implement Inter font.

---
+++ app/_app.tsx
@@ -0,0 +1,12 @@
+import { Inter } from 'next/font/google'
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] })
+
+export default function MyApp({ Component, pageProps }: any) {
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+    )
+}
