838361ce1938024edbb6d40376a3f593c9655a79
Tanmay Mazumdar <tanmay.mazumdar@quokkalabs.com>
2023-12-14 12:37:50 +0530

    (fix)[build]: Fix the deployment issue.

--- components/filter-pill.tsx
+++ components/filter-pill.tsx
@@ -18,25 +18,23 @@ export interface FilterPillProps {
   onChange?: (key: string) => void
 }
 
-export default function FilterPill(props: FilterPillProps): JSX.Element {
+export default function FilterPill(props: Readonly<FilterPillProps>): JSX.Element {
   const value = props.value
   const ref = useRef()
   const chip = useRef()
   const [open, setOpen] = useState(false)
-  
+
   function onStateChange(key: any): void {
     props.onChange?.([...key][0])
   }
-  
+
   return (
     <div>
       <div
-        className={
-          clsx(
-            'p-[5px] h-6 rounded-full border border-[#C1C8D1] flex gap-1 items-center text-[12px] text-[#6A7383] cursor-pointer bg-white',
-            value ? '' : 'border-dashed',
-          )
-        }
+        className={clsx(
+          'p-[5px] h-6 rounded-full border border-[#C1C8D1] flex gap-1 items-center text-[12px] text-[#6A7383] cursor-pointer bg-white',
+          value ? '' : 'border-dashed'
+        )}
         onClick={() => setOpen((prev: boolean) => !prev)}
         // @ts-expect-error
         ref={chip}
@@ -45,38 +43,60 @@ export default function FilterPill(props: FilterPillProps): JSX.Element {
         {props.label}
         {value ? (
           <>
-            {' '}| <p className='text-[#007AFF] text-xs font-medium capitalize'>{value}</p>
+            {' '}
+            | <p className='text-[#007AFF] text-xs font-medium capitalize'>{value}</p>
             <ArrowDownIcon />
           </>
         ) : null}
       </div>
       {/* @ts-expect-error */}
       <div ref={ref} />
-      
+
       {open
         ? createPortal(
-          <div className={clsx("flex flex-col gap-4 w-[300px] p-4 bg-white border border-gray-200 rounded-[10px] shadow absolute")} style={{ top: `${chip.current.getBoundingClientRect().bottom + 8}px` }}>
-              <h1 className="text-xl font-medium text-gray-900">Filter by {props.label}</h1>
+            <div
+              className={clsx(
+                'flex flex-col gap-4 w-[300px] p-4 bg-white border border-gray-200 rounded-[10px] shadow absolute'
+              )}
+              // @ts-expect-error
+              style={{ top: `${chip.current.getBoundingClientRect().bottom + 8}px` }}
+            >
+              <h1 className='text-xl font-medium text-gray-900'>Filter by {props.label}</h1>
               <div className='flex items-center gap-2 w-full'>
-                <p className="font-normal text-gray-700 min-w-[78px]">is equal to</p>
-                
-                {props.label === 'State'
-                  ? <Select 
-                      variant='bordered'
-                      placeholder='Select a state'
-                      className="max-w-xs"
-                      {...(value ? { defaultSelectedKeys: [(value ?? '').toLowerCase()] } : {})}
-                      onSelectionChange={onStateChange}
-                    >
-                      {states.map((state: {id:string,label:string}) => <SelectItem key={state.id} value={state.id}>{state.label}</SelectItem>)}
-                    </Select>
-                  : <Input label='' type='text' variant='bordered' value={value ?? ''} classNames={{inputWrapper: ['h-[34px]']}} />}
+                <p className='font-normal text-gray-700 min-w-[78px]'>is equal to</p>
+
+                {props.label === 'State' ? (
+                  <Select
+                    variant='bordered'
+                    placeholder='Select a state'
+                    className='max-w-xs'
+                    {...(value ? { defaultSelectedKeys: [(value ?? '').toLowerCase()] } : {})}
+                    onSelectionChange={onStateChange}
+                  >
+                    {states.map((state: { id: string; label: string }) => (
+                      <SelectItem key={state.id} value={state.id}>
+                        {state.label}
+                      </SelectItem>
+                    ))}
+                  </Select>
+                ) : (
+                  <Input
+                    label=''
+                    type='text'
+                    variant='bordered'
+                    value={value ?? ''}
+                    classNames={{ inputWrapper: ['h-[34px]'] }}
+                  />
+                )}
               </div>
-              <Button variant='normal' onClick={() => setOpen(false)}>Apply</Button>
-          </div>,
+              <Button variant='normal' onClick={() => setOpen(false)}>
+                Apply
+              </Button>
+            </div>,
             // @ts-expect-error
             ref.current
-          ) : null}
+          )
+        : null}
     </div>
   )
 }
--- components/states.tsx
+++ components/states.tsx
@@ -9,22 +9,22 @@ import PendingIcon from '../assets/pending.svg'
 
 export interface StatesProps extends ComponentProps<'div'> {
   label: string
-  state:  string
+  state: string
 }
 
 function getStateStyle(state: string): string | null {
   if (state === ESTATE.COMPLETE) {
     return 'bg-[#B8EF814D] border-[#B8EF81] text-[#478524]'
   }
-  
+
   if (state === ESTATE.DRAFT) {
     return 'bg-[#C1C8D14D] border-[#C1C8D1] text-[#6E7686]'
   }
-  
+
   if (state === ESTATE.PENDING) {
     return 'bg-[#FFE6634D] border-[#FFE663] text-[#FF9900]'
   }
-  
+
   return null
 }
 
@@ -32,23 +32,28 @@ function getStateIcon(state: string) {
   if (state === ESTATE.DRAFT) {
     return DraftIcon
   }
-  
+
   if (state === ESTATE.COMPLETE) {
     return CompletedIcon
   }
-  
+
   if (state === ESTATE.PENDING) {
     return PendingIcon
   }
 }
 
-export default function States(props: StatesProps): JSX.Element {
+export default function States(props: Readonly<StatesProps>): JSX.Element {
   const stateStyle = getStateStyle(props.state)
-  
+
   const StateIcon = getStateIcon(props.state)
-  
+
   return (
-    <div className={clsx(stateStyle, 'py-[3px] w-fit px-1 flex gap-1 items-center border rounded text-xs h-[20px] capitalize')}>
+    <div
+      className={clsx(
+        stateStyle,
+        'py-[3px] w-fit px-1 flex gap-1 items-center border rounded text-xs h-[20px] capitalize'
+      )}
+    >
       {props.label}
       <StateIcon />
     </div>
